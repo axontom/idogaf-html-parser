bf29d6d5108616d08acb2287a1fb14c47c2d649a
axontom <tomek.rusinowicz@gmail.com>
2020-08-11 02:05:44 +0200

    Basic implementation for Document class.

--- include/document.h
+++ include/document.h
@@ -4,6 +4,7 @@
 #ifndef DOCUMENT_H
 #define DOCUMENT_H
 
+#include <string>
 #include "element.h"
 
 namespace idogaf
@@ -25,8 +26,37 @@ class Document
          */
         Document& operator=(const Document& other);
 
+        //Getters
+        /** Get doctype of this document
+
+            @return Doctype of this document or an empty string if the doctype
+            has not been set.
+        */
+        std::string GetDoctype();
+        /** Get the root element of the document
+
+            @return Pointer to the root element of this document.
+        */
+        Element*    GetRoot();
+
+        //Setters
+        /** Set the root element of this document
+
+            Add given element as a new root of this document.
+            Removes any parent connections from a given element.
+
+            @param root Pointer to the element to be set as root.
+        */
+        void        SetRoot(Element* root);
+        /** Set doctype of this document
+
+            @param doctype Doctype to set.
+        */
+        void        SetDoctype(std::string doctype);
+
     protected:
-        Element&    root_;
+        Element*    root_;
+        std::string doctype_;
 
     private:
 };
--- src/document.cpp
+++ src/document.cpp
@@ -5,6 +5,16 @@ namespace idogaf
 Document::Document()
 {
     //ctor
+    root_ = nullptr;
+    doctype_ = "";
+}
+
+
+Document::Document(const Document& other)
+{
+    //copy ctor
+    root_ = other.root_;
+    doctype_ = other.doctype_;
 }
 
 Document::~Document()
@@ -12,15 +22,26 @@ Document::~Document()
     //dtor
 }
 
-Document::Document(const Document& other)
-{
-    //copy ctor
-}
-
 Document& Document::operator=(const Document& rhs)
 {
     if (this == &rhs) return *this; // handle self assignment
     //assignment operator
+    root_ = rhs.root_;
+    doctype_ = rhs.doctype_;
     return *this;
 }
+
+//Getters
+std::string Document::GetDoctype() { return doctype_; }
+Element* Document::GetRoot() { return root_; }
+
+//Setters
+void Document::SetDoctype(std::string doctype) { doctype_ = doctype; }
+void Document::SetRoot(Element* root)
+{
+    root_ = root;
+    if(root != nullptr)
+        root_->RemoveParent();
+}
+
 }
