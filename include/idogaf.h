6bf7dd858c45a2cdfb0c50a54bd4d30eba35fd57
axontom <tomek.rusinowicz@gmail.com>
2020-08-24 18:38:45 +0200

    Main header file for the library.

---
+++ include/idogaf.h
@@ -0,0 +1,14 @@
+#ifndef IDOGAF_H_INCLUDED
+#define IDOGAF_H_INCLUDED
+
+#include "attribute.h"
+#include "class.h"
+#include "document.h"
+#include "element.h"
+#include "id"
+#include "manager.h"
+#include "misc.h"
+#include "parser.h"
+#include "style.h"
+
+#endif // IDOGAF_H_INCLUDED
